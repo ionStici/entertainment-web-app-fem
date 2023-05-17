d7a04ace044013967108c89dba839ec9a8160325
Ion Stici <ionsticidev@gmail.com>
2023-05-17 09:44:30 +0300

    Update assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import './assets';
+import './styles/base.scss';
+import './styles/typography.scss';
+import data from './data.json';
+
 function App() {
-    return null;
+    return (
+        <>
+            <div></div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/assets.jsx
@@ -0,0 +1,320 @@
+// // // // // // // // // // // // // // // // // // // //
+
+import data from './data.json';
+
+// // // // // // // // // // // // // // // // // // // //
+
+import beyondEarthTrendingSmall from './assets/thumbnails/beyond-earth/trending/small.jpg';
+import beyondEarthTrendingLarge from './assets/thumbnails/beyond-earth/trending/large.jpg';
+
+import beyondEarthRegularSmall from './assets/thumbnails/beyond-earth/regular/small.jpg';
+import beyondEarthRegularMedium from './assets/thumbnails/beyond-earth/regular/medium.jpg';
+import beyondEarthRegularLarge from './assets/thumbnails/beyond-earth/regular/large.jpg';
+
+data[0].thumbnail.trending.small = beyondEarthTrendingSmall;
+data[0].thumbnail.trending.large = beyondEarthTrendingLarge;
+data[0].thumbnail.regular.small = beyondEarthRegularSmall;
+data[0].thumbnail.regular.medium = beyondEarthRegularMedium;
+data[0].thumbnail.regular.large = beyondEarthRegularLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import bottomGearTrendingSmall from './assets/thumbnails/bottom-gear/trending/small.jpg';
+import bottomGearTrendinglarge from './assets/thumbnails/bottom-gear/trending/large.jpg';
+
+import bottomGearRegularSmall from './assets/thumbnails/bottom-gear/regular/small.jpg';
+import bottomGearRegularMedium from './assets/thumbnails/bottom-gear/regular/medium.jpg';
+import bottomGearRegularLarge from './assets/thumbnails/bottom-gear/regular/large.jpg';
+
+data[1].thumbnail.trending.small = bottomGearTrendingSmall;
+data[1].thumbnail.trending.large = bottomGearTrendinglarge;
+data[1].thumbnail.regular.small = bottomGearRegularSmall;
+data[1].thumbnail.regular.medium = bottomGearRegularMedium;
+data[1].thumbnail.regular.large = bottomGearRegularLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import undiscoveredCitiesTrendingSmall from './assets/thumbnails/undiscovered-cities/trending/small.jpg';
+import undiscoveredCitiesTrendingLarge from './assets/thumbnails/undiscovered-cities/trending/large.jpg';
+
+import undiscoveredCitiesRegularSmall from './assets/thumbnails/undiscovered-cities/regular/small.jpg';
+import undiscoveredCitiesRegularMedium from './assets/thumbnails/undiscovered-cities/regular/medium.jpg';
+import undiscoveredCitiesRegularLarge from './assets/thumbnails/undiscovered-cities/regular/large.jpg';
+
+data[2].thumbnail.trending.small = undiscoveredCitiesTrendingSmall;
+data[2].thumbnail.trending.large = undiscoveredCitiesTrendingLarge;
+data[2].thumbnail.regular.small = undiscoveredCitiesRegularSmall;
+data[2].thumbnail.regular.medium = undiscoveredCitiesRegularMedium;
+data[2].thumbnail.regular.large = undiscoveredCitiesRegularLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import n1998TrendingSmall from './assets/thumbnails/1998/trending/small.jpg';
+import n1998TrendingLarge from './assets/thumbnails/1998/trending/large.jpg';
+
+import n1998RegularSmall from './assets/thumbnails/1998/regular/small.jpg';
+import n1998RegularMedium from './assets/thumbnails/1998/regular/medium.jpg';
+import n1998RegularLarge from './assets/thumbnails/1998/regular/large.jpg';
+
+data[3].thumbnail.trending.small = n1998TrendingSmall;
+data[3].thumbnail.trending.large = n1998TrendingLarge;
+data[3].thumbnail.regular.small = n1998RegularSmall;
+data[3].thumbnail.regular.medium = n1998RegularMedium;
+data[3].thumbnail.regular.large = n1998RegularLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import darkSideMoonTrendingSmall from './assets/thumbnails/dark-side-of-the-moon/trending/small.jpg';
+import darkSideMoonTrendingLarge from './assets/thumbnails/dark-side-of-the-moon/trending/large.jpg';
+
+import darkSideMoonRegularSmall from './assets/thumbnails/dark-side-of-the-moon/regular/small.jpg';
+import darkSideMoonRegularMedium from './assets/thumbnails/dark-side-of-the-moon/regular/medium.jpg';
+import darkSideMoonRegularLarge from './assets/thumbnails/dark-side-of-the-moon/regular/large.jpg';
+
+data[4].thumbnail.trending.small = darkSideMoonTrendingSmall;
+data[4].thumbnail.trending.large = darkSideMoonTrendingLarge;
+data[4].thumbnail.regular.small = darkSideMoonRegularSmall;
+data[4].thumbnail.regular.medium = darkSideMoonRegularMedium;
+data[4].thumbnail.regular.large = darkSideMoonRegularLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import greatLandsSmall from './assets/thumbnails/the-great-lands/regular/small.jpg';
+import greatLandsMedium from './assets/thumbnails/the-great-lands/regular/medium.jpg';
+import greatLandsLarge from './assets/thumbnails/the-great-lands/regular/large.jpg';
+
+data[5].thumbnail.regular.small = greatLandsSmall;
+data[5].thumbnail.regular.medium = greatLandsMedium;
+data[5].thumbnail.regular.large = greatLandsLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import theDiarySmall from './assets/thumbnails/the-diary/regular/small.jpg';
+import theDiaryMedium from './assets/thumbnails/the-diary/regular/medium.jpg';
+import theDiaryLarge from './assets/thumbnails/the-diary/regular/large.jpg';
+
+data[6].thumbnail.regular.small = theDiarySmall;
+data[6].thumbnail.regular.medium = theDiaryMedium;
+data[6].thumbnail.regular.large = theDiaryLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import earthUntouchedSmall from './assets/thumbnails/earths-untouched/regular/small.jpg';
+import earthUntouchedMedium from './assets/thumbnails/earths-untouched/regular/medium.jpg';
+import earthUntouchedLarge from './assets/thumbnails/earths-untouched/regular/large.jpg';
+
+data[7].thumbnail.regular.small = earthUntouchedSmall;
+data[7].thumbnail.regular.medium = earthUntouchedMedium;
+data[7].thumbnail.regular.large = earthUntouchedLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import noLandBeyondSmall from './assets/thumbnails/no-land-beyond/regular/small.jpg';
+import noLandBeyondMedium from './assets/thumbnails/no-land-beyond/regular/medium.jpg';
+import noLandBeyondLarge from './assets/thumbnails/no-land-beyond/regular/large.jpg';
+
+data[8].thumbnail.regular.small = noLandBeyondSmall;
+data[8].thumbnail.regular.medium = noLandBeyondMedium;
+data[8].thumbnail.regular.large = noLandBeyondLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import duringTheHuntSmall from './assets/thumbnails/during-the-hunt/regular/small.jpg';
+import duringTheHuntMedium from './assets/thumbnails/during-the-hunt/regular/medium.jpg';
+import duringTheHuntLarge from './assets/thumbnails/during-the-hunt/regular/large.jpg';
+
+data[9].thumbnail.regular.small = duringTheHuntSmall;
+data[9].thumbnail.regular.medium = duringTheHuntMedium;
+data[9].thumbnail.regular.large = duringTheHuntLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import autosportTheSeriesSmall from './assets/thumbnails/autosport-the-series/regular/small.jpg';
+import autosportTheSeriesMedium from './assets/thumbnails/autosport-the-series/regular/medium.jpg';
+import autosportTheSeriesLarge from './assets/thumbnails/autosport-the-series/regular/large.jpg';
+
+data[10].thumbnail.regular.small = autosportTheSeriesSmall;
+data[10].thumbnail.regular.medium = autosportTheSeriesMedium;
+data[10].thumbnail.regular.large = autosportTheSeriesLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import sameAnserSmall from './assets/thumbnails/same-answer-2/regular/small.jpg';
+import sameAnserMedium from './assets/thumbnails/same-answer-2/regular/medium.jpg';
+import sameAnserLarge from './assets/thumbnails/same-answer-2/regular/large.jpg';
+
+data[11].thumbnail.regular.small = sameAnserSmall;
+data[11].thumbnail.regular.medium = sameAnserMedium;
+data[11].thumbnail.regular.large = sameAnserLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import belowEchoSmall from './assets/thumbnails/below-echo/regular/small.jpg';
+import belowEchoMedium from './assets/thumbnails/below-echo/regular/medium.jpg';
+import belowEchoLarge from './assets/thumbnails/below-echo/regular/large.jpg';
+
+data[12].thumbnail.regular.small = belowEchoSmall;
+data[12].thumbnail.regular.medium = belowEchoMedium;
+data[12].thumbnail.regular.large = belowEchoLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import theRockiesSmall from './assets/thumbnails/the-rockies/regular/small.jpg';
+import theRockiesMedium from './assets/thumbnails/the-rockies/regular/medium.jpg';
+import theRockiesLarge from './assets/thumbnails/the-rockies/regular/large.jpg';
+
+data[13].thumbnail.regular.small = theRockiesSmall;
+data[13].thumbnail.regular.medium = theRockiesMedium;
+data[13].thumbnail.regular.large = theRockiesLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import relentlessSmall from './assets/thumbnails/relentless/regular/small.jpg';
+import relentlessMedium from './assets/thumbnails/relentless/regular/medium.jpg';
+import relentlessLarge from './assets/thumbnails/relentless/regular/large.jpg';
+
+data[14].thumbnail.regular.small = relentlessSmall;
+data[14].thumbnail.regular.medium = relentlessMedium;
+data[14].thumbnail.regular.large = relentlessLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import communityOfOursSmall from './assets/thumbnails/community-of-ours/regular/small.jpg';
+import communityOfOursMedium from './assets/thumbnails/community-of-ours/regular/medium.jpg';
+import communityOfOursLarge from './assets/thumbnails/community-of-ours/regular/large.jpg';
+
+data[15].thumbnail.regular.small = communityOfOursSmall;
+data[15].thumbnail.regular.medium = communityOfOursMedium;
+data[15].thumbnail.regular.large = communityOfOursLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import vanLifeSmall from './assets/thumbnails/van-life/regular/small.jpg';
+import vanLifeMedium from './assets/thumbnails/van-life/regular/medium.jpg';
+import vanLifeLarge from './assets/thumbnails/van-life/regular/large.jpg';
+
+data[16].thumbnail.regular.small = vanLifeSmall;
+data[16].thumbnail.regular.medium = vanLifeMedium;
+data[16].thumbnail.regular.large = vanLifeLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import theHeiressSmall from './assets/thumbnails/the-heiress/regular/small.jpg';
+import theHeiressMedium from './assets/thumbnails/the-heiress/regular/medium.jpg';
+import theHeiressLarge from './assets/thumbnails/the-heiress/regular/large.jpg';
+
+data[17].thumbnail.regular.small = theHeiressSmall;
+data[17].thumbnail.regular.medium = theHeiressMedium;
+data[17].thumbnail.regular.large = theHeiressLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import offTheTrackSmall from './assets/thumbnails/off-the-track/regular/small.jpg';
+import offTheTrackMedium from './assets/thumbnails/off-the-track/regular/medium.jpg';
+import offTheTrackLarge from './assets/thumbnails/off-the-track/regular/large.jpg';
+
+data[18].thumbnail.regular.small = offTheTrackSmall;
+data[18].thumbnail.regular.medium = offTheTrackMedium;
+data[18].thumbnail.regular.large = offTheTrackLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import whisperingHillSmall from './assets/thumbnails/whispering-hill/regular/small.jpg';
+import whisperingHillMedium from './assets/thumbnails/whispering-hill/regular/medium.jpg';
+import whisperingHillLarge from './assets/thumbnails/whispering-hill/regular/large.jpg';
+
+data[19].thumbnail.regular.small = whisperingHillSmall;
+data[19].thumbnail.regular.medium = whisperingHillMedium;
+data[19].thumbnail.regular.large = whisperingHillLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import n112Small from './assets/thumbnails/112/regular/small.jpg';
+import n112Medium from './assets/thumbnails/112/regular/medium.jpg';
+import n112Large from './assets/thumbnails/112/regular/large.jpg';
+
+data[20].thumbnail.regular.small = n112Small;
+data[20].thumbnail.regular.medium = n112Medium;
+data[20].thumbnail.regular.large = n112Large;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import loneHeartSmall from './assets/thumbnails/lone-heart/regular/small.jpg';
+import loneHeartMedium from './assets/thumbnails/lone-heart/regular/medium.jpg';
+import loneHeartLarge from './assets/thumbnails/lone-heart/regular/large.jpg';
+
+data[21].thumbnail.regular.small = loneHeartSmall;
+data[21].thumbnail.regular.medium = loneHeartMedium;
+data[21].thumbnail.regular.large = loneHeartLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import productionLineSmall from './assets/thumbnails/production-line/regular/small.jpg';
+import productionLineMedium from './assets/thumbnails/production-line/regular/medium.jpg';
+import productionLineLarge from './assets/thumbnails/production-line/regular/large.jpg';
+
+data[22].thumbnail.regular.small = productionLineSmall;
+data[22].thumbnail.regular.medium = productionLineMedium;
+data[22].thumbnail.regular.large = productionLineLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import dogsSmall from './assets/thumbnails/dogs/regular/small.jpg';
+import dogsMedium from './assets/thumbnails/dogs/regular/medium.jpg';
+import dogsLarge from './assets/thumbnails/dogs/regular/large.jpg';
+
+data[23].thumbnail.regular.small = dogsSmall;
+data[23].thumbnail.regular.medium = dogsMedium;
+data[23].thumbnail.regular.large = dogsLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import asiaIn24DaysSmall from './assets/thumbnails/asia-in-24-days/regular/small.jpg';
+import asiaIn24DaysMedium from './assets/thumbnails/asia-in-24-days/regular/medium.jpg';
+import asiaIn24DaysLarge from './assets/thumbnails/asia-in-24-days/regular/large.jpg';
+
+data[24].thumbnail.regular.small = asiaIn24DaysSmall;
+data[24].thumbnail.regular.medium = asiaIn24DaysMedium;
+data[24].thumbnail.regular.large = asiaIn24DaysLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import theTastyTourSmall from './assets/thumbnails/the-tasty-tour/regular/small.jpg';
+import theTastyTourMedium from './assets/thumbnails/the-tasty-tour/regular/small.jpg';
+import theTastyTourLarge from './assets/thumbnails/the-tasty-tour/regular/small.jpg';
+
+data[25].thumbnail.regular.small = theTastyTourSmall;
+data[25].thumbnail.regular.medium = theTastyTourMedium;
+data[25].thumbnail.regular.large = theTastyTourLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import darkerSmall from './assets/thumbnails/darker/regular/small.jpg';
+import darkerMedium from './assets/thumbnails/darker/regular/medium.jpg';
+import darkerLarge from './assets/thumbnails/darker/regular/large.jpg';
+
+data[26].thumbnail.regular.small = darkerSmall;
+data[26].thumbnail.regular.medium = darkerMedium;
+data[26].thumbnail.regular.large = darkerLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import unresolvedCasesSmall from './assets/thumbnails/unresolved-cases/regular/small.jpg';
+import unresolvedCasesMedium from './assets/thumbnails/unresolved-cases/regular/medium.jpg';
+import unresolvedCasesLarge from './assets/thumbnails/unresolved-cases/regular/large.jpg';
+
+data[27].thumbnail.regular.small = unresolvedCasesSmall;
+data[27].thumbnail.regular.medium = unresolvedCasesMedium;
+data[27].thumbnail.regular.large = unresolvedCasesLarge;
+
+// // // // // // // // // // // // // // // // // // // //
+
+import missionSaturnSmall from './assets/thumbnails/mission-saturn/regular/small.jpg';
+import missionSaturnMedium from './assets/thumbnails/mission-saturn/regular/medium.jpg';
+import missionSaturnLarge from './assets/thumbnails/mission-saturn/regular/large.jpg';
+
+data[28].thumbnail.regular.small = missionSaturnSmall;
+data[28].thumbnail.regular.medium = missionSaturnMedium;
+data[28].thumbnail.regular.large = missionSaturnLarge;
+
+// // // // // // // // // // // // // // // // // // // //
