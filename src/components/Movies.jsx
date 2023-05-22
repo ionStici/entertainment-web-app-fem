b61b9ddcaba9cbaab519e9c161d7e4a549c1e3af
Ion Stici <ionsticidev@gmail.com>
2023-05-22 13:06:46 +0300

    Improve Movie Component

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,9 +1,57 @@
+import React from 'react';
 import styles from './../styles/Movies.module.scss';
 import PropTypes from 'prop-types';
 
 const Movies = function (props) {
     const assets = props.assets;
     const movies = props.movies;
+
+    const handleBookmarkMouseOver = function ({ target }) {
+        const icon = target.querySelector('img');
+        icon.classList.add(styles.bookmark_button_hover);
+
+        if (icon.dataset.isbookmarked === 'false') {
+            icon.src = assets.iconBookmarkFull;
+        }
+
+        if (icon.dataset.isbookmarked === 'true') {
+            icon.src = assets.iconBookmarkEmpty;
+        }
+    };
+    const handleBookmarkMouseOut = function ({ target }) {
+        const icon = target.querySelector('img');
+        icon.classList.remove(styles.bookmark_button_hover);
+
+        if (!(icon.dataset.isbookmarked === 'false')) {
+            icon.src = assets.iconBookmarkFull;
+        }
+
+        if (!(icon.dataset.isbookmarked === 'true')) {
+            icon.src = assets.iconBookmarkEmpty;
+        }
+    };
+    const [bookmark, updateBookmark] = React.useState(0);
+    const handleBookmarkClick = function ({ target }) {
+        const icon = target.querySelector('img');
+        icon.classList.remove(styles.bookmark_button_hover);
+
+        const movie = movies.find(movie => {
+            return movie.title === target.dataset.movie;
+        });
+
+        updateBookmark(prev => prev + 1);
+        bookmark;
+
+        if (movie.isBookmarked === false) {
+            movie.isBookmarked = true;
+            return;
+        }
+
+        if (movie.isBookmarked === true) {
+            movie.isBookmarked = false;
+            return;
+        }
+    };
 
     return (
         <>
@@ -26,8 +74,19 @@ const Movies = function (props) {
                                         </picture>
 
                                         {/* prettier-ignore */}
-                                        <button className={styles.bookmark_button} aria-label="Bookmark"><img src={movie.isBookmarked ? assets.iconBookmarkFull : assets.iconBookmarkEmpty} alt="Bookmark" data-isbookmarked={movie.isBookmarked} /></button>
+                                        <button onClick={handleBookmarkClick} onMouseOver={handleBookmarkMouseOver} onMouseOut={handleBookmarkMouseOut} className={styles.bookmark_button} aria-label="Bookmark" data-movie={movie.title}><img src={movie.isBookmarked ? assets.iconBookmarkFull : assets.iconBookmarkEmpty} alt="Bookmark" data-isbookmarked={movie.isBookmarked} /></button>
                                     </div>
+
+                                    <div className={styles.box_details}>
+                                        <p>{movie.year}</p>
+                                        {/* prettier-ignore */}
+                                        <p><img src={movie.category === 'Movie' ? assets.iconCategoryMovie : assets.iconCategoryTv} alt={movie.category} /><span>{movie.category}</span></p>
+                                        <p>{movie.rating}</p>
+                                    </div>
+
+                                    <h2 className={styles.movie_title}>
+                                        {movie.title}
+                                    </h2>
                                 </div>
                             );
                         })}
